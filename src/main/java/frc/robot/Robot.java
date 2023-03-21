661658f6caca8eff05949dcc1b727a0ffb3063c4
Jacob1010-h <lordgaming1010@gmail.com>
2023-03-21 15:56:10 -0700

    remove all test code and add basic driver input

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,52 +5,46 @@
 
 package frc.robot;
 
-import calc.Constants;
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import hardware.Swerve;
 
 
 public class Robot extends TimedRobot {
-    // TODO: remove test code
-    private final XboxController controller = new XboxController(0);
+    private final XboxController driver = new XboxController(0);
     private final Swerve swerve = new Swerve();
 
-    // Slew rate limiters to make joystick inputs more gentle; 1/3 sec from 0 to 1.
-    private final SlewRateLimiter xSpeedLimiter = new SlewRateLimiter(3);
-    private final SlewRateLimiter ySpeedLimiter = new SlewRateLimiter(3);
-    private final SlewRateLimiter rotateLimiter = new SlewRateLimiter(3);
+    @Override
+    public void robotInit() {
 
+    }
 
     @Override
-    public void autonomousPeriodic() {
-        driveWithJoystick(false);
-        swerve.calibrateOdometry();
+    public void robotPeriodic() {
+
+    }
+
+    @Override
+    public void autonomousInit() {}
+    @Override
+    public void autonomousPeriodic() {}
+
+    @Override
+    public void teleopInit() {
+
     }
 
     @Override
     public void teleopPeriodic() {
-        driveWithJoystick(true);
+        swerve.drive(driver.getLeftY(), driver.getLeftX(), driver.getRightX(), true, driver.getAButton());
     }
 
-    // TODO: remove test code and add calculation to drive method
-    private void driveWithJoystick(boolean fieldRelative) {
-        // Get the x speed. We are inverting this because Xbox controllers return
-        // negative values when we push forward.
-        final var xSpeed = -xSpeedLimiter.calculate(controller.getLeftY()) * Constants.DriveConstants.MAX_SPEED;
-
-        // Get the y speed or sideways/strafe speed. We are inverting this because
-        // we want a positive value when we pull to the left. Xbox controllers
-        // return positive values when you pull to the right by default.
-        final var ySpeed = -ySpeedLimiter.calculate(controller.getLeftX()) * Constants.DriveConstants.MAX_SPEED;
-
-        // Get the rate of angular rotation. We are inverting this because we want a
-        // positive value when we pull to the left (remember, CCW is positive in
-        // mathematics). Xbox controllers return positive values when you pull to
-        // the right by default.
-        final var rot = -rotateLimiter.calculate(controller.getRightX()) * Constants.DriveConstants.MAX_ANGULAR_SPEED;
-
-        swerve.drive(xSpeed, ySpeed, rot, fieldRelative);
-    }
+    @Override
+    public void testInit() {}
+    @Override
+    public void testPeriodic() {}
+    @Override
+    public void disabledInit() {}
+    @Override
+    public void disabledPeriodic() {}
 }
